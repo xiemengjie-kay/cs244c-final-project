70026cafffdcd16025898386e5213129ef013964
Mengjie Xie <kaylee@kaylee-lt.beluga-snapper.ts.net>
2026-03-06 16:14:51 -0800

    add gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kaylee/Kaylee/Stf/Classes/CS244C/FinalProject
-# Build directory: /Users/kaylee/Kaylee/Stf/Classes/CS244C/FinalProject/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[paxos_tests]=] "/Users/kaylee/Kaylee/Stf/Classes/CS244C/FinalProject/build/paxos_tests")
-set_tests_properties([=[paxos_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/kaylee/Kaylee/Stf/Classes/CS244C/FinalProject/CMakeLists.txt;22;add_test;/Users/kaylee/Kaylee/Stf/Classes/CS244C/FinalProject/CMakeLists.txt;0;")
